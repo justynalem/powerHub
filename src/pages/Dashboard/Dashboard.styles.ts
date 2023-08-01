d99422e035641cf6d8a6c559459ed553db7bfa78
Justyna <lempickajustyna@gmail.com>
2023-08-01 10:42:23 +0200

    dashboard changes

---
+++ src/pages/Dashboard/Dashboard.styles.ts
@@ -0,0 +1,30 @@
+import { Box } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import { drawerWidth } from "../../theme";
+
+export const DashboardContainer = styled(Box)(() => ({
+  width: "100vw",
+  height: "100vh",
+}));
+
+export const MainContainer = styled(Box)(()=>({
+  width: "100%",
+  height: "100%"
+}))
+
+export const MapContainer = styled(Box)(()=>({
+  width: "100%",
+  height: "100%",
+  zIndex: "1",
+  position: "relative"
+}))
+
+export const SliderContainer = styled(Box)(({theme})=>({
+  position: "absolute",
+  bottom: 0,
+  zIndex: 2,
+  transition: theme.transitions.create("all", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  })
+}))
---
+++ src/pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,38 @@
+import { Map } from "../../components";
+import { Drawer } from "../../ui";
+import { StationContainer } from "../../ui/StationBox/StationBoxContainer";
+import { useState } from "react";
+import { drawerWidth } from "../../theme";
+import {
+  DashboardContainer,
+  MainContainer,
+  MapContainer,
+  SliderContainer,
+} from "./Dashboard.styles";
+
+export const Dashboard = () => {
+  const [isDrawerOpened, setIsDrawerOpened] = useState(false);
+
+  const onDrawerOpenChange = (isOpen: boolean) => {
+    setIsDrawerOpened(isOpen);
+  };
+
+  return (
+    <DashboardContainer>
+      <Drawer onOpenChange={onDrawerOpenChange} />
+      <MainContainer>
+        <MapContainer>
+          <Map />
+        </MapContainer>
+        <SliderContainer
+          sx={{
+            width: isDrawerOpened ? `calc(100% - ${drawerWidth})` : "calc(100% - 65px)",
+            backgroundColor: "black",
+            transform: isDrawerOpened ? `translateX(${drawerWidth})` : "translateX(65px)",
+          }}>
+          <StationContainer />
+        </SliderContainer>
+      </MainContainer>
+    </DashboardContainer>
+  );
+};
